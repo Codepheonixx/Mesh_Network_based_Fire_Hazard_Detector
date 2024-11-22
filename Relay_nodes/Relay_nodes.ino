c5d28ddc4470803de3044af85cc2f5ed83b8a7fe
Soumyadeep Bala <144486135+Codepheonixx@users.noreply.github.com>
2024-11-23 03:10:01 +0530

    Create Relay_nodes.ino

---
+++ Relay_nodes/Relay_nodes.ino
@@ -0,0 +1,26 @@
+//*******************************
+//This is the implementation of the nodes for communicating and exchanging info with each other.
+//*******************************
+
+
+//importing painlessMesh library and defining passwords and port for mesh networking
+#include "painlessMesh.h"  
+
+#define  MESH_PREFIX    "Fire_Gas_Detector"
+#define  MESH_PASSWORD  "saveslifefromfire"
+#define  MESH_PORT      5555
+
+// Adding Scheduler for personal tasks
+Scheduler myscheduler;
+painlessMesh mesh;
+
+// Creating tasks for scheduled duration
+
+
+void setup(){
+
+}
+
+void loop(){
+
+}
